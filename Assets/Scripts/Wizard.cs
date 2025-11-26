baf42d7da24269f0b6e362be2fb8b8bd300d25da
SandroCJ210 <sandro.carrillo.j@uni.pe>
2025-11-25 19:24:52 -0500

    feat: add empty Player and Wizard classes

---
+++ Assets/Scripts/Wizard.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Wizard : Gambler
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void PlayTurn()
+    {
+        
+    }
+
+    protected override void DrawCard()
+    {
+        
+    }
+
+    protected override void Pass()
+    {
+        
+    }
+}
